5d791137bfa7baf7f2ce803a1f4a2e7dcb2d2d97
qiaoyu <qiaoyu1111358@gmail.com>
2017-06-20 17:56:39 +0800

    implement backen CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   root 'welcome#index'
 
+  namespace :admin do
+    resources :products
+  end
+
 end
