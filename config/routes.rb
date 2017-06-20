f1e4d6fb3de5e414dc1d65b4e69e3b216e13a774
qiaoyu <qiaoyu1111358@gmail.com>
2017-06-20 18:35:22 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
-  root 'welcome#index'
+  #root 'welcome#index'
+  root 'products#index'
 
   namespace :admin do
     resources :products
   end
+
+  resources :products
 
 end
